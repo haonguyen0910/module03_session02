0213afd81e3bc6e515f90948c452a18fed7a8498
Hao Nguyen <thaihao0910@gmail.com>
2026-05-17 15:45:11 +0700

    update bài 3

--- src/main/java/com/example/taskmanagement/controllers/TaskController.java
+++ src/main/java/com/example/taskmanagement/controllers/TaskController.java
@@ -3,11 +3,17 @@ package com.example.taskmanagement.controllers;
 import com.example.taskmanagement.models.Task;
 import com.example.taskmanagement.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
     private TaskService taskService;
 
@@ -16,7 +22,20 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    public List<Task> findAllTasks() {
-        return taskService.findAllTasks();
+    @GetMapping
+    public ResponseEntity<List<Task>> findAllTasks(@RequestParam(required = false) String search) {
+        List<Task> tasks = taskService.findAllTasks();
+
+        if(search != null && !search.isEmpty()) {
+            List<Task> result = new ArrayList<>();
+
+            for (Task x : tasks) {
+                if (x.getTitle().toLowerCase().contains(search.toLowerCase())) {
+                    result.add(x);
+                }
+            }
+            tasks = result;
+        }
+        return ResponseEntity.ok(tasks);
     }
 }
--- src/main/java/com/example/taskmanagement/controllers/UserController.java
+++ src/main/java/com/example/taskmanagement/controllers/UserController.java
@@ -3,11 +3,18 @@ package com.example.taskmanagement.controllers;
 import com.example.taskmanagement.models.User;
 import com.example.taskmanagement.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private UserService userService;
 
@@ -16,7 +23,20 @@ public class UserController {
         this.userService = userService;
     }
 
-    public List<User> findAllUsers() {
-        return userService.findAllUsers();
+    @GetMapping
+    public ResponseEntity<List<User>> findAllUsers(@RequestParam(required = false) String search) {
+        List<User> users = userService.findAllUsers();
+
+        if(search != null && !search.isEmpty()) {
+            List<User> result = new ArrayList<>();
+
+            for (User x : users) {
+                if(x.getName().toLowerCase().contains(search.toLowerCase())) {
+                    result.add(x);
+                }
+            }
+            users = result;
+        }
+        return ResponseEntity.ok(users);
     }
 }
